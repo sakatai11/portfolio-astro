ef7a304f7dcdd2b29f491f6ecf3c1c7c35ad40d6
Sakai Taichi <130420550+sakatai11@users.noreply.github.com>
2025-12-06 13:42:48 +0900

    モックファイルの定義

---
+++ src/mocks/constants.ts
@@ -0,0 +1,54 @@
+export const CONTENTS = [
+  { id: 1, src: "https://picsum.photos/seed/content1/600/600" },
+  { id: 2, src: "https://picsum.photos/seed/content2/600/600" },
+  { id: 3, src: "https://picsum.photos/seed/content3/600/600" },
+  { id: 4, src: "https://picsum.photos/seed/content4/600/600" },
+  { id: 5, src: "https://picsum.photos/seed/content5/600/600" },
+  { id: 6, src: "https://picsum.photos/seed/content6/600/600" },
+  { id: 7, src: "https://picsum.photos/seed/content7/600/600" },
+  { id: 8, src: "https://picsum.photos/seed/content8/600/600" },
+];
+
+export const PHOTOS = [
+  {
+    id: 1,
+    src: "https://picsum.photos/seed/list1/1200/800",
+    title: "タイトル",
+    label: ["旅行"],
+    date: "2024.10.10",
+  },
+  {
+    id: 2,
+    src: "https://picsum.photos/seed/list2/1200/800",
+    title: "タイトル",
+    label: ["旅行", "雑記"],
+    date: "2024.10.10",
+  },
+  {
+    id: 3,
+    src: "https://picsum.photos/seed/list3/1200/800",
+    title: "タイトル",
+    label: ["雑記"],
+    date: "2024.10.10",
+  },
+  {
+    id: 4,
+    src: "https://picsum.photos/seed/list4/1200/800",
+    title: "タイトル",
+    label: ["スポーツ"],
+    date: "2024.10.10",
+  },
+  {
+    id: 5,
+    src: "https://picsum.photos/seed/list5/1200/800",
+    title: "タイトル",
+    label: ["スポーツ", "雑記"],
+    date: "2024.10.10",
+  },
+];
+
+export const EVENTS = [
+  { year: "2025", image: "https://picsum.photos/seed/timeline1/800/500" },
+  { year: "2024", image: "https://picsum.photos/seed/timeline2/800/500" },
+  { year: "2023", image: "https://picsum.photos/seed/timeline3/800/500" },
+];
