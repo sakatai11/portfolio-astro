0da69787f0513ba3a616e3583a6d8e5c2f7f8c47
Sakai Taichi <130420550+sakatai11@users.noreply.github.com>
2026-01-23 22:04:10 +0900

    モック更新

--- src/mocks/constants.ts
+++ src/mocks/constants.ts
@@ -1,12 +1,12 @@
 export const CONTENTS = [
-  { id: 1, src: "https://picsum.photos/seed/content1/600/600" },
-  { id: 2, src: "https://picsum.photos/seed/content2/600/600" },
-  { id: 3, src: "https://picsum.photos/seed/content3/600/600" },
-  { id: 4, src: "https://picsum.photos/seed/content4/600/600" },
-  { id: 5, src: "https://picsum.photos/seed/content5/600/600" },
-  { id: 6, src: "https://picsum.photos/seed/content6/600/600" },
-  { id: 7, src: "https://picsum.photos/seed/content7/600/600" },
-  { id: 8, src: "https://picsum.photos/seed/content8/600/600" },
+  { id: 1, src: "https://picsum.photos/seed/content1/1365/2048" },
+  { id: 2, src: "https://picsum.photos/seed/content2/1365/2048" },
+  { id: 3, src: "https://picsum.photos/seed/content3/1365/2048" },
+  { id: 4, src: "https://picsum.photos/seed/content4/1365/2048" },
+  { id: 5, src: "https://picsum.photos/seed/content5/1365/2048" },
+  { id: 6, src: "https://picsum.photos/seed/content6/1365/2048" },
+  { id: 7, src: "https://picsum.photos/seed/content7/1365/2048" },
+  { id: 8, src: "https://picsum.photos/seed/content8/1365/2048" },
 ];
 
 export const DETAILED_CONTENTS = [
@@ -205,35 +205,35 @@ export const DETAILED_CONTENTS = [
 export const PHOTOS = [
   {
     id: 1,
-    src: "https://picsum.photos/seed/list1/1200/800",
+    src: "https://picsum.photos/seed/list1/2048/1365",
     title: "タイトル",
     label: ["旅行"],
     date: "2024.10.10",
   },
   {
     id: 2,
-    src: "https://picsum.photos/seed/list2/1200/800",
+    src: "https://picsum.photos/seed/list2/2048/1365",
     title: "タイトル",
     label: ["旅行", "雑記"],
     date: "2024.10.10",
   },
   {
     id: 3,
-    src: "https://picsum.photos/seed/list3/1200/800",
+    src: "https://picsum.photos/seed/list3/2048/1365",
     title: "タイトル",
     label: ["雑記"],
     date: "2024.10.10",
   },
   {
     id: 4,
-    src: "https://picsum.photos/seed/list4/1200/800",
+    src: "https://picsum.photos/seed/list4/2048/1365",
     title: "タイトル",
     label: ["スポーツ"],
     date: "2024.10.10",
   },
   {
     id: 5,
-    src: "https://picsum.photos/seed/list5/1200/800",
+    src: "https://picsum.photos/seed/list5/2048/1365",
     title: "タイトル",
     label: ["スポーツ", "雑記"],
     date: "2024.10.10",
